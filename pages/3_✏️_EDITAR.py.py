bf0a6280d4d9ae5760af81c3013db7b282090494
fcf2025 <fcf.energy@gmail.com>
2026-05-25 14:02:48 -0300

    Update 3_✏️_EDITAR.py.py

--- pages/3_✏️_EDITAR.py.py
+++ pages/3_✏️_EDITAR.py.py
@@ -77,6 +77,24 @@ tipos = ["", "Artigo", "Livro", "Capítulo", "Dissertação", "Tese", "Relatóri
 paises = ["", "Brasil", "Portugal", "Estados Unidos", "Reino Unido", "França", "Alemanha", "Espanha", "Itália", "China", "Japão", "Outro"]
 idiomas = ["", "Português", "Inglês", "Espanhol", "Francês", "Alemão", "Outro"]
 temas = ["", "Planejamento Urbano", "Meio Ambiente", "Mobilidade", "Habitação", "Geoprocessamento", "Legislação", "Outros"]
+doi = ["Revista",
+            "Journal",
+            "Periódico",
+            "Conferência",
+            "Livro",
+            "Capítulo de Livro",
+            "Site",
+            "Repositório",
+            "Anais de Congresso / Proceedings",
+            "Boletim Técnico",
+            "Newsletter",
+            "Plataforma Digital Acadêmica (ex.: ResearchGate, Academia.edu)",
+            "Biblioteca Digital (ex.: BDTD, Scielo, JSTOR)",
+            "Base de Dados Estatística (ex.: IBGE, ONU Data)",
+            "Documento Oficial / Institucional (ex.: ONU, OMS, IBGE)",
+            "Enciclopédia / Dicionário Especializado",
+            "Blog Técnico ou Acadêmico",
+            "Outro"]
 
 
 # Funções auxiliares para encontrar o índice atual nos selects
