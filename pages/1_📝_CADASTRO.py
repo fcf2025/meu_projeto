d453afd8530f8ca4bd7b8fd3dc535b693f003c8a
fcf2025 <fcf.energy@gmail.com>
2026-06-11 14:06:14 -0300

    Remove duplicate entry from list of categories

--- pages/1_📝_CADASTRO.py
+++ pages/1_📝_CADASTRO.py
@@ -46,7 +46,7 @@ LISTA_TIPOS = [
                 "Boletim Técnico / Informativo",
                 "Artigo de Opinião / Editorial",
                 "Entrevista / Depoimento",
-                "Outros", "Outro"]
+                "Outros"]
 LISTA_TEMAS = ["", "Financiamento", "Tarifa (Taxas de drenagem)", "Custos (Operacionais e de Implantação)","Taxas","Regulação e Governança","Soluções Baseadas na Natureza (SbN) e Infraestrutura Verde",
                 "Planejamento Urbano e Uso do Solo","Sustentabilidade e Mudanças Climáticas","Tecnologias de Monitoramento", "Cidades Inteligentes (Smart Cities)",                    
                 "Investimentos em DMAPU","Cidades Inteligentes","Manejo de Águas Pluviais Urbanas (MAPU)",
